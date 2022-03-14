64c04cceb77449a53afae8032f4dbf2816c9d731
lucetre <lucetre@naver.com>
2022-03-14 17:43:52 +0900

    PIR_LED

--- DEMO.md
+++ DEMO.md
@@ -2,5 +2,8 @@
 ### 1. Connect a LED via pin #8
 <video src="./LED_BLINK/demo.mp4"></video>
 
-### 2. Connect 3 LEDs via pin #8, #9, #10
+### 2. Connect three LEDs via pin #8, #9, #10
 <video src="./3_LED_BLINK/demo.mp4"></video>
+
+### 3. Connect PIR sensor and a LED via pin #11 and #8 w.r.t
+<video src="./PIR_LED/demo.mp4"></video>
--- TEST/TEST.ino
+++ TEST/TEST.ino
@@ -1,24 +1,8 @@
-#define PIN1 8
-#define PIN2 9
-#define PIN3 10
 
 void setup() {
-  pinMode(PIN1, OUTPUT);
-  pinMode(PIN2, OUTPUT);
-  pinMode(PIN3, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(PIN1, HIGH);
-  digitalWrite(PIN2, LOW);
-  digitalWrite(PIN3, LOW);
-  delay(500);
-  digitalWrite(PIN1, LOW);
-  digitalWrite(PIN2, HIGH);
-  digitalWrite(PIN3, LOW);
-  delay(500);
-  digitalWrite(PIN1, LOW);
-  digitalWrite(PIN2, LOW);
-  digitalWrite(PIN3, HIGH);
-  delay(500);
+  Serial.println("Hello World");
 }
