d5c63bffc59e730ac1c19e215727c4cc427f87e0
lucetre <lucetre@naver.com>
2022-03-31 11:03:26 +0900

    Scripts for serial monitor

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,24 @@
+Help() {
+   echo "-p   Set serial board rate"
+   echo "-s   Show serial monitor"
+}
+
 scripts/compile.sh $1
-scripts/upload.sh $1
-scripts/serial.sh 9600
+scripts/upload.sh $1 
+shift
+
+Rate=9600
+while getopts ":hsp:" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      p)
+         Rate=$OPTARG;;
+      s)
+         scripts/serial.sh -p $Rate;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
--- scripts/serial.sh
+++ scripts/serial.sh
@@ -1,1 +1,20 @@
-putty COM3 -serial -sercfg $1,8,n,1,N &
+Help() {
+   echo "-p   Set serial board rate"
+}
+
+Rate=9600
+
+while getopts ":hp:" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      p)
+         Rate=$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+putty COM3 -serial -sercfg $Rate,8,n,1,N &
